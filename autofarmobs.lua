2d7f41bfe638e053216fbaaf0a1847e2c5a03a71
Trollz <gtacrew535@gmail.com>
2026-05-24 18:02:14 -0500

    Update autofarmobs.lua

--- autofarmobs.lua
+++ autofarmobs.lua
@@ -11,7 +11,7 @@ local autoEquipEnabled = true
 local hopEnabled = true
 local performanceBoostEnabled = true
 
-local hopAfterSeconds = 120  -- Change this if you want different time
+local hopAfterSeconds = 60  -- Change this if you want different time
 -- =================================================
 
 local disableAllGUIs = true
